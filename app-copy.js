ace1f2d162e39af92c26b687a567a11a10f0d7d1
fau <fau.guswanto@gmail.com>
2025-11-21 16:58:01 +0700

    update for learning

---
+++ app-copy.js
@@ -0,0 +1,34 @@
+const todoForm = document.querySelector("form");
+const todoInput = document.querySelector("#todo-input");
+const todoListUl = document.querySelector("#todo-list");
+
+let allTodos = [];
+updateTodoListUI();
+
+todoForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    addTodo();
+})
+
+function addTodo() {
+    const todoText = todoInput.value.trim();
+    if (todoText.length > 0) {
+        allTodos.push(todoText);
+        updateTodoListUI();
+        todoInput.value = "";
+    }
+}
+
+function updateTodoListUI() {
+    todoListUl.innerHTML = "";
+    allTodos.forEach((todo, todoIndex) => {
+        todoItem = createTodoItem(todo, todoIndex)
+        todoListUl.append(todoItem);
+    })
+}
+
+function createTodoItem (todoText) {
+    const todoLi = document.createElement("li");
+    todoLi.innerText = todoText;
+    return todoLi;
+}
--- app.js
+++ app.js
@@ -6,29 +6,30 @@ let allTodos = [];
 updateTodoListUI();
 
 todoForm.addEventListener("submit", function(e){
-    e.preventDefault();
-    addTodo();
+    e.preventDefault();                         // untuk membuat form tidak refresh ketika diinput
+    addTodo();                                  // memanggil fungsi untuk menambahkan list todo ketika submit form todo
 })
 
 function addTodo() {
-    const todoText = todoInput.value.trim();
-    if (todoText.length > 0) {
-        allTodos.push(todoText);
-        updateTodoListUI();
-        todoInput.value = "";
+    const todoText = todoInput.value.trim();    // membuat variable todoText untuk menampung hasil input dari todoInput dan sudah di trim
+    if (todoText.length > 0) {                  // kalau length atau isi karakter dari todoInput lebih dari 0
+        allTodos.push(todoText);                // push todoText ke array allTodos
+        updateTodoListUI();                     // update tampilan todo
+        todoInput.value = "";                   // hapus isi kolom todoInput
     }
 }
 
 function updateTodoListUI() {
     todoListUl.innerHTML = "";
-    allTodos.forEach((todo, todoIndex) => {
-        todoItem = createTodoItem(todo, todoIndex)
+
+    allTodos.forEach(todo => {
+        const todoItem = createTodoItem(todo);
         todoListUl.append(todoItem);
     })
 }
 
 function createTodoItem (todoText) {
-    const todoLi = document.createElement("li");
-    todoLi.innerText = todoText;
-    return todoLi;
+    const li = document.createElement("li");
+    li.innerText = todoText;
+    return li;
 }
