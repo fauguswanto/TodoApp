2811e5e1722a11f1639716a4c344016c6f83ab96
fau <fau.guswanto@gmail.com>
2025-11-21 22:23:15 +0700

    update for todo application improvements

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
-const todoForm = document.querySelector("form");
+const todoForm = document.querySelector("form")
 const todoInput = document.querySelector("#todo-input");
 const todoListUl = document.querySelector("#todo-list");
 
 let allTodos = [];
 updateTodoListUI();
 
-todoForm.addEventListener("submit", function(e){
-    e.preventDefault();                         // untuk membuat form tidak refresh ketika diinput
-    addTodo();                                  // memanggil fungsi untuk menambahkan list todo ketika submit form todo
+todoForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    addTodo();
 })
 
 function addTodo() {
-    const todoText = todoInput.value.trim();    // membuat variable todoText untuk menampung hasil input dari todoInput dan sudah di trim
-    if (todoText.length > 0) {                  // kalau length atau isi karakter dari todoInput lebih dari 0
-        allTodos.push(todoText);                // push todoText ke array allTodos
-        updateTodoListUI();                     // update tampilan todo
-        todoInput.value = "";                   // hapus isi kolom todoInput
+    let todoText = todoInput.value.trim();
+    if (todoText.length > 0) {
+        allTodos.push(todoText);
+        updateTodoListUI();
+        todoInput.value = "";
     }
 }
 
 function updateTodoListUI() {
     todoListUl.innerHTML = "";
-
-    allTodos.forEach(todo => {
-        const todoItem = createTodoItem(todo);
+    allTodos.forEach((todo, todoIndex) =>{
+        todoItem = createTodoItem(todo, todoIndex)
         todoListUl.append(todoItem);
     })
 }
 
-function createTodoItem (todoText) {
-    const li = document.createElement("li");
-    li.innerText = todoText;
-    return li;
+function createTodoItem(todoText, todoIndex) {
+    let todoLi = document.createElement("li");
+    todoLi.className = "todo";
+    todoLi.innerHTML = `
+    <input type="checkbox" id="todo-1">
+                <label for="todo-1" class="custom-checkbox">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+                </label>
+                <label for="todo-1" class="todo-text">
+                    ${todoText}
+                </label>
+                <button class="delete-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                </button>`
+    return todoLi;
 }
